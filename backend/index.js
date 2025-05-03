34756d5c917861b493c9a7e5f235839cbf342127
Anik-kumar <anik.kumar.sarker@gmail.com>
2025-05-03 20:51:56 +0600

    Added basic backend crud apis

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from 'cors';
 import { PORT, MongoDBURL } from "./config.js";
-import { Book } from './models/bookModel.js';
+import bookRoutes from "./routes/bookRoutes.js";
 // import { Book as iBook} from './interfaces/book.js';
 
 
 const app = express();
+
+// middleware for parsing req body
+app.use(express.json());
+
+// middleware for cors
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(234).send("Welcome to the page");
+});
+
+app.use("/api/books", bookRoutes);
 
 mongoose
   .connect(MongoDBURL)
@@ -21,34 +40,6 @@ mongoose
   });
 
 
-app.get('/', (req, res) => {
-  res.status(234).send("Welcome to the page");
-});
-
-app.post('/books', async (req, res) => {
-  try {
-    if (
-      !req.body.title ||
-      !req.body.author ||
-      !req.body.publishYear
-    ) {
-      return res.status(400).send({ message: "title, author, publishYear are required fields"});
-    }
-
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear
-    };
-
-    const book = await Book.create(newBook);
-    
-    return res.status(201).send(book);
-  } catch(err) {
-    console.log(err.message);
-    res.status(500).send({message: err.message});
-  }
-});
 
 
 
